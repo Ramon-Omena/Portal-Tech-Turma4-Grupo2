1e32852d19602c38a115bff47d83ed8fa4ef719d
Adecorsino <adecorsino@gmail.com>
2023-02-01 17:41:24 -0300

    btn

--- ade-redondonews/Script.js
+++ ade-redondonews/Script.js
@@ -1,15 +1,17 @@
 function leiaMais(){
-  var pontos=document.getElementById("pontos");
-  var maisTexto=document.getElementById("mais");
-  var btnLeiaMais=document.getElementById("btnLeiaMais");
+  var pontos = document.getElementById("pontos");
+  var maisTexto = document.getElementById("mais");
+  var btnLeiaMais = document.getElementById("btnLeiaMais");
 
-  if(pontos.style.display === "nome"){
-    pontos.style.display="inline";
-    maisTexto.style.display="nome";
-    btnLeiaMais.innerHTML="Leia Mais";
-  }else{ 
-    pontos.style.display="none";
-    maisTexto.style.display="inline";
-    btnLeiaMais.innerHTML="Leia Menos";
+  if(pontos.style.display === "inline"){
+    pontos.style.display = "none";
+    maisTexto.style.display = "inline";
+    btnLeiaMais.innerHTML = "Leia Menos";
+  }
+  else {
+    pontos.style.display = "inline";
+    maisTexto.style.display = "none";
+    btnLeiaMais.innerHTML = "Leia Mais";
   }
 }
+
