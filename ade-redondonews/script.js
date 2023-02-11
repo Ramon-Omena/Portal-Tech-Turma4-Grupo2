8d048d447be67d487882a403ca39171e6bac7f49
Adecorsino <adecorsino@gmail.com>
2023-02-11 19:15:42 -0300

    finaliando6

--- ade-redondonews/script.js
+++ ade-redondonews/script.js
@@ -1,3 +1,21 @@
+function leiaMais(){
+  var pontos = document.getElementById("pontos");
+  var maisTexto = document.getElementById("mais");
+  var btnLeiaMais = document.getElementById("btnLeiaMais");
+
+  if(pontos.style.display === "inline"){
+    pontos.style.display = "none";
+    maisTexto.style.display = "inline";
+    btnLeiaMais.innerHTML = "Leia Menos";
+  }
+  else {
+    pontos.style.display = "inline";
+    maisTexto.style.display = "none";
+    btnLeiaMais.innerHTML = "Leia Mais";
+  }
+}
+
+
 // ---------- FUNÇÕES GERAIS -------------- //
 function togglePopup(input, label) {
   // Mostrar popup de campo obrigatório
