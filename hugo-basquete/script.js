58e9c3793b9cab9f443c451f3102a40865cfa06e
hugofSoares <hugofs18@gmail.com>
2023-02-13 10:18:51 -0300

    testando script

--- hugo-basquete/script.js
+++ hugo-basquete/script.js
@@ -1,5 +1,5 @@
 /*TABELA MUDA DE COR LINHAS */
-var tabela=document.getElementById("tabela")
+var tabela = document.getElementById("tabela")
     
 for(var celula in tabela.getElementsByTagName("tr")){
 tabela.getElementsByTagName("tr")[celula].onmouseover=function(){this.style.background="#C1EAD2"}
@@ -12,25 +12,20 @@ tabela.getElementsByTagName("tr")[celula].onmouseout=function(){this.style.backg
 
 
 ///testando validacao de submit
-
 /* let btnSubmit = document.querySelector('button[type="submit"]');
-let inputsCorretos = {
-time: checked,
-time2: checked
-}
-btnSubmit.addEventListener("click", (e)=>{if("input[name='time1']:checked"||"input[name='time2']:checked")
-{
+
+btnSubmit.addEventListener("click", (e)=>{
+    if (document.getElementById("r1") || document.getElementById("r2").checked) {
     e.preventDefault();
-    console.log(inputsCorretos);
-    alert("Mensagem enviada com sucesso!")
+    
+    alert("Selecione um time!!!")
     } else {
-    alert("Favor Selecione um Time!");
-}
-})
- */
+    alert("Mensagem enviada com sucesso!");
+    }
+}) */
 
-
-$(function() {
+////teste tres falhou///
+/* $(function() {
     $("#btn_enviar").click(function() {
        $(".caixa").css("background-color", "white");
  
@@ -43,4 +38,32 @@ $(function() {
        alert("Formulário validado!");
        return true;
     });
- });
+ }); */
+
+
+
+ /* testando submit  */
+
+
+/*  document.getElementById("btn_enviar").onclick = function() {
+    var times = document.getElementsByName("time");
+    for (var i = 0; i < times.length; i++) {
+        if (times[i].checked) {
+            alert("Msg Enviada");
+        }
+        else{
+            alert("Escolha um TIME!")
+        }
+    }
+};  */
+
+let btnSubmit = document.querySelector('button[type="submit"]');
+
+btnSubmit.addEventListener("click", (e)=>{
+if (document.getElementsByName("time") == true ){
+    alert("É necessário preencher todos os campos corretamente!")
+    } 
+else {
+    alert("Mensagem enviada com sucesso!");
+    }
+})
